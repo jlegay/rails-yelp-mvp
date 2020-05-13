b2032434bac059820152f74f65392d7c1895ce3c
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-05-13 11:29:38 +0200

    Created the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_085705) do
+ActiveRecord::Schema.define(version: 2020_05_13_091208) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
